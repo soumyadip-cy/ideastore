e1bf6aa08202fb98cb0cf84270dc49caa8f01cff
soumyadip-cy <207708452+soumyadip-cy@users.noreply.github.com>
2025-12-23 00:27:04 +0530

    Fixed BASE URL dynamic logic

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === 'development' ? "http://localhost:5001/api" : "https://ideastore-backend.onrender.com/api";
+//This uses the Vite environment variable MODE to set the base URL for axios requests based on the mode (development or production). MODE=development when using 'npm run dev' and MODE=production when using 'npm run build' and 'npm run preview'.
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 
 const api = axios.create({
     baseURL: BASE_URL,
